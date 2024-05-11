9a6a5af0a5846b7780981606029d16af32182bbf
Sourav Pradhan <whitepegasus1603@gmail.com>
2024-05-11 15:01:57 +0530

    added fault tolerance using, circuit-breaker + retry + rate-limiter

--- jobms/src/main/java/com/sourav/jobms/service/impl/JobServiceImpl.java
+++ jobms/src/main/java/com/sourav/jobms/service/impl/JobServiceImpl.java
@@ -1,5 +1,6 @@
 package com.sourav.jobms.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -18,77 +19,91 @@ import com.sourav.jobms.model.Job;
 import com.sourav.jobms.repository.JobRepository;
 import com.sourav.jobms.service.JobService;
 
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
+import io.github.resilience4j.retry.annotation.Retry;
+
 @Service
 public class JobServiceImpl implements JobService {
-    // private List<Job> jobs = new ArrayList<>();
-    JobRepository jobRepository;
+	// private List<Job> jobs = new ArrayList<>();
+	JobRepository jobRepository;
+	
+	int attempt = 0;
 
-    @Autowired
-    RestTemplate restTemplate;
+	@Autowired
+	RestTemplate restTemplate;
 
-    private CompanyClient companyClient;
-    private ReviewClient reviewClient;
+	private CompanyClient companyClient;
+	private ReviewClient reviewClient;
 
-    public JobServiceImpl(JobRepository jobRepository, CompanyClient companyClient,
-                          ReviewClient reviewClient) {
-        this.jobRepository = jobRepository;
-        this.companyClient = companyClient;
-        this.reviewClient = reviewClient;
-    }
+	public JobServiceImpl(JobRepository jobRepository, CompanyClient companyClient, ReviewClient reviewClient) {
+		this.jobRepository = jobRepository;
+		this.companyClient = companyClient;
+		this.reviewClient = reviewClient;
+	}
 
-    @Override
-    public List<JobDTO> findAll() {
-        List<Job> jobs = jobRepository.findAll();
+	@Override
+//  @CircuitBreaker(name = "companyBreaker",
+//          fallbackMethod = "companyBreakerFallback")
+//	@Retry(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+	@RateLimiter(name = "companyBreaker", fallbackMethod = "companyBreakerFallback")
+	public List<JobDTO> findAll() {
+		System.out.println("Attempt: " + ++attempt);
+		List<Job> jobs = jobRepository.findAll();
+//		List<JobDTO> jobDTOS = new ArrayList<>();
 
-        return jobs.stream().map(this::convertToDto)
-                .collect(Collectors.toList());
-    }
+		return jobs.stream().map(this::convertToDto).collect(Collectors.toList());
+	}
 
-    private JobDTO convertToDto(Job job) {
-        Company company = companyClient.getCompany(job.getCompanyId());
-        List<Review> reviews = reviewClient.getReviews(job.getCompanyId());
+	public List<String> companyBreakerFallback(Exception e) {
+		List<String> list = new ArrayList<>();
+		list.add("Dummy");
+		return list;
+	}
 
-        JobDTO jobDTO = JobMapper.
-                mapToJobWithCompanyDto(job,company,reviews);
-        //jobDTO.setCompany(company);
+	private JobDTO convertToDto(Job job) {
+		Company company = companyClient.getCompany(job.getCompanyId());
+		List<Review> reviews = reviewClient.getReviews(job.getCompanyId());
 
-        return jobDTO;
-    }
+		JobDTO jobDTO = JobMapper.mapToJobWithCompanyDto(job, company, reviews);
+		// jobDTO.setCompany(company);
 
-    @Override
-    public void createJob(Job job) {
-        jobRepository.save(job);
-    }
+		return jobDTO;
+	}
 
-    @Override
-    public JobDTO getJobById(Long id) {
-        Job job = jobRepository.findById(id).orElse(null);
-        return convertToDto(job);
-    }
+	@Override
+	public void createJob(Job job) {
+		jobRepository.save(job);
+	}
 
-    @Override
-    public boolean deleteJobById(Long id) {
-        try {
-            jobRepository.deleteById(id);
-            return true;
-        } catch (Exception e) {
-            return false;
-        }
-    }
+	@Override
+	public JobDTO getJobById(Long id) {
+		Job job = jobRepository.findById(id).orElse(null);
+		return convertToDto(job);
+	}
 
-    @Override
-    public boolean updateJob(Long id, Job updatedJob) {
-        Optional<Job> jobOptional = jobRepository.findById(id);
-        if (jobOptional.isPresent()) {
-            Job job = jobOptional.get();
-            job.setTitle(updatedJob.getTitle());
-            job.setDescription(updatedJob.getDescription());
-            job.setMinSalary(updatedJob.getMinSalary());
-            job.setMaxSalary(updatedJob.getMaxSalary());
-            job.setLocation(updatedJob.getLocation());
-            jobRepository.save(job);
-            return true;
-        }
-        return false;
-    }
+	@Override
+	public boolean deleteJobById(Long id) {
+		try {
+			jobRepository.deleteById(id);
+			return true;
+		} catch (Exception e) {
+			return false;
+		}
+	}
+
+	@Override
+	public boolean updateJob(Long id, Job updatedJob) {
+		Optional<Job> jobOptional = jobRepository.findById(id);
+		if (jobOptional.isPresent()) {
+			Job job = jobOptional.get();
+			job.setTitle(updatedJob.getTitle());
+			job.setDescription(updatedJob.getDescription());
+			job.setMinSalary(updatedJob.getMinSalary());
+			job.setMaxSalary(updatedJob.getMaxSalary());
+			job.setLocation(updatedJob.getLocation());
+			jobRepository.save(job);
+			return true;
+		}
+		return false;
+	}
 }
